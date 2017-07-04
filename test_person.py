7106e69ae53d8182e95be5f64e9094e587422945
rogerclarkgc <rogerclark@163.com>
2017-07-04 18:08:44 +0800

    MODIFY[roger]:change some test samples

--- test_person.py
+++ test_person.py
@@ -1,13 +1,20 @@
 from html_screen import PersonalInfo
+from html_screen import get_personal_result
 
-fp = open('personal_result_dev.html', 'r', encoding='utf-8')
-personal_dev = fp.read()
+fp = open('other.html', 'r', encoding='utf-8')
+personal_page = fp.read()
+personal_dev = get_personal_result(personal_page)
 fp.close()
 
 Pinfo = PersonalInfo(personal_dev)
 frames = Pinfo.get_basic_frame()
 print(frames)
 
-info = Pinfo.get_personal_info(frames)
+all_info = Pinfo.get_all_info()
+#info = Pinfo.get_personal_info(frames)
+#contact = Pinfo.get_contact_info(frames)
+#education = Pinfo.get_education_info(frames)
+#work = Pinfo.get_work_info(frames)
 
-print(info)
+print(all_info)
+#print(dict(info, **contact, **education, **work))
