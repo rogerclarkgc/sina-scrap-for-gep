c9ff9bacd2ff0000a94e6e51742a9dcd2035ad20
rogerclarkgc <rogerclark@163.com>
2017-07-11 19:33:11 +0800

    START[roger]: add logger module

---
+++ logger.py
@@ -0,0 +1,29 @@
+log = {
+    'login_sina': {
+        'login_time': None,
+        'login_status': None,
+        'login_user': None,
+        'failure_reason': None,
+    },
+    'add_cookies': {
+        'add_time': None,
+        'cookie_owner': None,
+        'insert_status': None,
+    },
+    'fetch_cookies': {
+        'fetch_time': None,
+        'cookie_owner': None,
+        'cookie_name': None,
+        'failure_reason': None,
+    },
+    'get_search_result': {
+        'screen_time': None,
+        'screen_status': None
+    },
+    'get_page': {
+        'url': None,
+        'cookie_owner': None,
+        'need_login': None,
+        'failure_reason': None,
+    }
+}
