a97b91da01b2ff721ac55fc72639a29c9fd30ed6
rogerclarkgc <rogerclark@163.com>
2017-08-24 17:43:13 +0800

    ADD[roger]: add more vectorize model and add simi model

--- analysis.py
+++ analysis.py
@@ -5,8 +5,10 @@ import nltk
 import wordcloud
 import jieba
 from matplotlib import pyplot as plt
+import numpy as np
+from scipy import stats
 from gensim import similarities
-from gensim.models import Word2Vec, LdaModel, LsiModel, tfidfmodel
+from gensim.models import Word2Vec, LdaModel, LsiModel, tfidfmodel, RpModel, HdpModel
 from gensim.models.word2vec import LineSentence
 from gensim.corpora import Dictionary
 
@@ -143,16 +145,28 @@ class WordVector(object):
     4.以此为依据来进行分类和打分
     """
 class SenSimi(object):
+    """
+    This class can calculate the similarity of sentence
+    """
 
     def __init__(self, cg=None):
+        """
+        initiate the class
+        :param cg: the generator from class:Corpus
+        """
         import logging
         logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
         logging.root.setLevel(level=logging.INFO)
         self.cg = cg
+        self.bowlist = None
         self.word_dict = None
 
 
     def reconstructdata(self):
+        """
+        return word list and remove the word only appear 1 times
+        :return: the word list after reconstruct
+        """
 
         freq = defaultdict(int)
         print('generate freq list for every words.')
@@ -164,16 +178,141 @@ class SenSimi(object):
         return raw
 
     def bowcorpus(self, raw=None):
+        """
+        return the list of bag of words and buld dictionary
+        :param raw: the raw list from reconstructdata
+        :return: the list of bag of words
+        """
         print('generating dictionary')
         self.word_dict = Dictionary(raw)
         print('generating the list of bow...')
         bowlist = [self.word_dict.doc2bow(sentence) for sentence in raw]
+        self.bowlist = bowlist
         return bowlist
 
-    def tfidfmodel(self, bowlist=None, save=False):
+    def tfidfmodel(self, bowlist=None, save=False, savename=None):
+        """
+        build a tfidfmodel
+        :param bowlist: the list of bag of words
+        :param save: save the model
+        :param savename: the file name of model
+        :return: the model itself
+        """
         print('using Tfidfmodel')
         tfidf = tfidfmodel.TfidfModel(bowlist)
+        if save:
+            tfidf.save(savename)
         return tfidf
+    def lsimodel(self, corpus_t=None, topic=200, save=False, savename=None):
+        """
+
+        :param tfidf:
+        :param topic:
+        :return:
+        """
+        print('using Lsimodel...')
+        lsimodel = LsiModel(corpus=corpus_t, id2word=self.word_dict, num_topics=topic)
+        if save:
+            lsimodel.save(savename)
+        return lsimodel
+
+    def ldamodel(self, corpus_t=None, topic=200, save=False, savename=None):
+        """
+
+        :param tfidf:
+        :param topic:
+        :param save:
+        :param savename:
+        :return:
+        """
+        print('using Ldamodel...')
+        ldamodel = LdaModel(corpus=corpus_t, id2word=self.word_dict, num_topics=topic)
+        if save:
+            ldamodel.save(savename)
+        return ldamodel
+
+    def rpmodel(self, corpus_t=None, topic=200, save=False, savename=None):
+        """
+
+        :param corpus_t:
+        :param topic:
+        :param save:
+        :param savename:
+        :return:
+        """
+        print('using Random Projections model...')
+        rpmodel =RpModel(corpus=corpus_t, id2word=self.word_dict, num_topics=topic)
+        if save:
+            rpmodel.save(savename)
+        return rpmodel
+
+    def hdpmodel(self, corpus_t, save=False, savename=None):
+        """
+
+        :param corpus_t:
+        :param save:
+        :param savename:
+        :return:
+        """
+        print('using Hierarchical Dirichlet Process model...')
+        hdpmodel = HdpModel(corpus=corpus_t, id2word=self.word_dict)
+        if save:
+            hdpmodel.save(savename)
+        return hdpmodel
+    def indexcompare(self, query=None, model=None, topic=200):
+
+        tfidf_model = self.tfidfmodel(self.bowlist)
+        corpus_tfidf = tfidf_model[self.bowlist]
+
+        if model == 'lsi':
+            vectorspace = self.lsimodel(corpus_t=corpus_tfidf, topic=topic)
+
+        elif model == 'lda':
+            # FIXME: very slow when generating index
+            vectorspace = self.ldamodel(corpus_t=corpus_tfidf, topic=topic)
+        elif model == 'rp':
+            vectorspace = self.rpmodel(corpus_t=corpus_tfidf, topic=topic)
+        elif model == 'hdp':
+            vectorspace = self.hdpmodel(corpus_t=corpus_tfidf)
+        else:
+            raise RuntimeError('unrecognized model name')
+
+        corpus_proj = vectorspace[corpus_tfidf]
+        print('generating index matrix')
+        corpus_index = similarities.MatrixSimilarity(corpus_proj, num_features=topic)
+
+        query_bow = self.word_dict.doc2bow(query)
+        query_tfidf = tfidf_model[query_bow]
+        query_proj = vectorspace[query_tfidf]
+        simi = corpus_index[query_proj]
+        simi_list = list(simi)
+
+        return simi_list
+
+    def simihist(self, prob=True, bins=100, simi=None):
+
+        num_bins = bins
+        fig, ax = plt.subplots()
+
+        n, bins, patches = ax.hist(simi, num_bins, normed=prob)
+
+        print('starting Kernel density estimation....')
+        kde = stats.gaussian_kde(simi)
+        left = min(simi) - abs(0.5 * min(simi))
+        right = 1.2
+        x = np.arange(left, right, step=0.01)
+        y = kde(x)
+        ax.plot(x, y, '--')
+        ax.set_xlabel('Similarity')
+        ax.set_ylabel('Probability density')
+        print('producing plot...')
+        fig.tight_layout()
+        plt.show()
+
+
+
+
+
 
 
 
@@ -194,8 +333,20 @@ if __name__ == '__main__':
     #wf = WordFreq(res, 'freqdist')
     #summary = wf.freqdist()
     #print(summary)
-    wv = WordVector('panda_cut_string.txt')
-    model = wv.train(save=True, savename='panda_nonstop_model.vector')
-    print(model.wv.most_similar('大熊猫'))
+    #wv = WordVector('panda_cut_string.txt')
+    #model = wv.train(save=True, savename='panda_nonstop_model.vector')
+    #print(model.wv.most_similar('大熊猫'))
+    FILENAME = 'panda_corpus.txt'
+    panda_g = corpus(FILENAME)
+    si = SenSimi(panda_g)
+    panda_raw = si.reconstructdata()
+    bowlist = si.bowcorpus(panda_raw)
+
+    good = ['可爱', '萌', '喜欢', '国宝', '神奇']
+    res = si.indexcompare(query=good, model='lsi')
+    cleandata.writepkl(res, 'panda_simi824.pickle')
+    si.simihist(simi=res)
+    #print(max(res))
+    #print(len(res))
 
 
--- analysis_t.py
+++ analysis_t.py
@@ -1,4 +1,5 @@
 from gensim import similarities
+from gensim.models import LdaModel, LsiModel, tfidfmodel
 
 from analysis import *
 
@@ -16,17 +17,19 @@ if __name__ == '__main__':
     bowlist = si.bowcorpus(panda_raw)
     print(bowlist[1])
 
-    panda_tfidf = si.tfidfmodel(bowlist)
+    panda_tfidfmodel = si.tfidfmodel(bowlist)
+    panda_tfidf = panda_tfidfmodel[bowlist]
     # FIXME：不能用全部的语料生成索引，超出numpy.array限制
     # FIXME: 如果用部分语料，用于比较相似的句子不包括在索引矩阵的特征向量中（基）
-    panda_res = panda_tfidf[bowlist[:2]]
-    index = similarities.MatrixSimilarity(panda_res)
-
+    print('using lsi model...')
+    panda_lsi = LsiModel(corpus=panda_tfidf, id2word=si.word_dict, num_topics=300)
+    index = similarities.MatrixSimilarity(panda_lsi[panda_tfidf])
     good = ['可爱','萌', '喜欢','国宝','神奇']
     good_bow = si.word_dict.doc2bow(good)
-    good_tfidf = panda_tfidf[good_bow]
-    simi = index[good_tfidf]
+    good_tfidf = panda_tfidfmodel[good_bow]
+    good_lsi = panda_lsi[good_tfidf]
+    simi = index[good_lsi]
     simi_list = list(simi)
     print(max(simi_list))
-    print(good_tfidf)
-    print(good_bow)
+    where = simi_list.index(max(simi_list))
+    print(panda_raw[where])
